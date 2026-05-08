de5b2e604f77b749481a1b7d0bc91bab3056e1ac
tarek-elsayed <tarek.elsayed.morsi@gmail.com>
2026-05-08 19:35:01 +0300

    finsh him

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ var groupInput = document.getElementById("contactGroup");
 var noteInput = document.getElementById("contactNotes");
 var favouriteInput = document.getElementById("contactFavorite");
 var emergencyInput = document.getElementById("contactEmergency");
-var searchInput = document.getElementById('search');
+var searchInput = document.getElementById("search");
 var contactsList = [];
 
 var favouriteList = [];
 
 var emergancyList = [];
+
+var filterContact = [];
 
 checkDataStored();
 
@@ -26,30 +28,20 @@ function resetAllInputs() {
   noteInput.value = "";
   favouriteInput.value = "";
   emergencyInput.value = "";
+  searchInput.value = "";
 }
 function checkDataStored() {
-  contactsList = JSON.parse(localStorage.getItem("listOfContact"));
-  favouriteList = JSON.parse(localStorage.getItem("favList"));
-  emergancyList = JSON.parse(localStorage.getItem("emgList"));
-
-  if (contactsList !== null) {
-    displayContats(contactsList);
-  } else {
-    contactsList = [];
-    displayContats(contactsList);
-  }
-  if (favouriteList !== null) {
-    //   displayContats(favouriteList);
-  } else {
-    favouriteList = [];
-    //   displayContats(favouriteList);
-  }
-  if (emergancyList !== null) {
-    //   displayContats(emergancyList);
-  } else {
-    emergancyList = [];
-    //   displayContats(emergancyList);
-  }
+  contactsList = JSON.parse(localStorage.getItem("listOfContact"))
+    ? JSON.parse(localStorage.getItem("listOfContact"))
+    : [];
+  favouriteList = JSON.parse(localStorage.getItem("favList"))
+    ? JSON.parse(localStorage.getItem("favList"))
+    : [];
+  emergancyList = JSON.parse(localStorage.getItem("emgList"))
+    ? JSON.parse(localStorage.getItem("emgList"))
+    : [];
+  displayContats(contactsList);
+ 
 }
 document.getElementById("total-contact").innerHTML = contactsList.length;
 document.getElementById("total-contact-fav").innerHTML = favouriteList.length;
@@ -87,24 +79,41 @@ function addContact() {
       favourite: favouriteInput.checked,
       emergency: emergencyInput.checked,
     };
-    console.log(contact);
     contactsList.push(contact);
-    console.log(contactsList);
 
     localStorage.setItem("listOfContact", JSON.stringify(contactsList));
-
-    displayContats(contactsList);
+    if (contact.favourite) {
+      favouriteList.push(contact);
+      localStorage.setItem("favList", JSON.stringify(favouriteList));
+    }
+    if (contact.emergency) {
+      emergancyList.push(contact);
+      localStorage.setItem("emgList", JSON.stringify(emergancyList));
+    }
     resetAllInputs();
-    let modal = bootstrap.Modal.getInstance(
+    displayContats(contactsList);
+    var modal = bootstrap.Modal.getInstance(
       document.getElementById("exampleModal"),
     );
     modal.hide();
 
     document.getElementById("total-contact").innerHTML = contactsList.length;
+    Swal.fire({
+      title: "Added!",
+      text: "Contact has been add successfully.",
+      icon: "success",
+      draggable: true,
+    });
   }
+ 
 }
 
 function displayContats(contacts) {
+  if (filterContact.length !== 0) {
+    contacts = filterContact;
+  } else {
+    contacts = contactsList;
+  }
   if (contacts.length === 0) {
     document.getElementById("rowContactLeft").innerHTML = `<div class="row g-3">
       <div class="col-12 text-center py-5">
@@ -125,13 +134,13 @@ function displayContats(contacts) {
   }
 
   var cartona = "";
-  for (let i = 0; i < contacts.length; i++) {
+  for (var i = 0; i < contacts.length; i++) {
     cartona += `
    
                   <div class="col-12 col-sm-12 col-md-6 col-lg-6 contact-card">
                     <div class="contact-header d-flex gap-3">
                       <div class="header-logo">
-                        <p class="sami-name">TE</p>
+                        <p class="sami-name">${getInitials(contacts[i].name)}</p>
                         <div class="contact-icon">
                           <div class="icon icon-fav ${contacts[i].favourite == true ? "d-block" : "d-none"}">
                             <i class="fa-solid fa-star"></i>
@@ -204,10 +213,10 @@ function displayContats(contacts) {
                           <a id='emg-2' class="icon-emergancy btn-icon ${contacts[i].emergency == true ? "d-block" : "d-none"} solid-emg-icon" onclick='addEmergancy(${contacts[i].id})'
                             ><i class="fa-solid fa-heart-pulse"></i
                           ></a>
-                          <a class="icon-edit btn-icon"
+                          <a class="icon-edit btn-icon" onclick='putContactDate(${i})'
                             ><i class="fa-solid fa-pen"></i
                           ></a>
-                          <a class="icon-delete btn-icon"
+                          <a class="icon-delete btn-icon" onclick='deleteContact(${contacts[i].id})'
                             ><i class="fa-solid fa-trash"></i
                           ></a>
                         </div>
@@ -217,17 +226,13 @@ function displayContats(contacts) {
     `;
   }
   document.getElementById("rowContactLeft").innerHTML = cartona;
-  //   for (let i = 0; i < contacts.length; i++) {
-  //     if (contacts[i].emergency) {
-  //       document.getElementById("emg-1").classList.add("d-none");
-  //       document.getElementById("emg-2").classList.add("d-block");
-  //       document.getElementById("emg-2").classList.remove("d-none");
-  //     } else {
-  //       document.getElementById("emg-2").classList.add("d-none");
-  //       document.getElementById("emg-1").classList.remove("d-none");
-  //       document.getElementById("emg-1").classList.add("d-block");
-  //     }
-  //   }
+  document.getElementById("total-contact").innerHTML = contactsList.length;
+  document.getElementById("total-contact-fav").innerHTML = favouriteList.length;
+  document.getElementById("total-contact-emer").innerHTML =
+    emergancyList.length;
+  document.getElementById("totalContact").innerHTML = contactsList.length;
+  displayFavContacts();
+  displayEmgContacts();
 }
 
 function isNameValid() {
@@ -269,23 +274,28 @@ function isEmailValid() {
   }
 }
 
-function addFavourite(id) {
-  for (let i = 0; i < contactsList.length; i++) {
+function addFavourite(id, flag) {
+  for (var i = 0; i < contactsList.length; i++) {
     if (contactsList[i].id == id) {
-      contactsList[i].favourite = !contactsList[i].favourite;
-      //   contactsList[i].favourite = contactsList[i].favourite === false ? true : false;
-      if (contactsList[i].favourite) {
+      flag == "update"
+        ? (contactsList[i].favourite = contactsList[i].favourite)
+        : (contactsList[i].favourite = !contactsList[i].favourite);
+      
+       if ((contactsList[i].favourite && (favouriteList.length !== 0 && favouriteList[i].id !== id)) ||  favouriteList.length == 0) {
         favouriteList.push(contactsList[i]);
         localStorage.setItem("favList", JSON.stringify(favouriteList));
         displayContats(contactsList);
-      } else {
+      }else if (contactsList[i].favourite &&(favouriteList.length !==0 && favouriteList[i].id == id)){
+        displayContats(contactsList);
+      } 
+      else {
         removeFavourite(id);
       }
     }
   }
 }
 function removeFavourite(id) {
-  for (let i = 0; i < favouriteList.length; i++) {
+  for (var i = 0; i < favouriteList.length; i++) {
     if (favouriteList[i].id == id) {
       favouriteList.splice(i, 1);
       localStorage.setItem("favList", JSON.stringify(favouriteList));
@@ -294,20 +304,25 @@ function removeFavourite(id) {
   displayContats(contactsList);
 }
 
-function addEmergancy(id) {
-  for (let i = 0; i < contactsList.length; i++) {
+function addEmergancy(id, flag) {
+  for (var i = 0; i < contactsList.length; i++) {
     console.log(emergancyList);
 
     if (contactsList[i].id == id) {
-      //   contactsList[i].emergency = !contactsList[i].emergency;
-      contactsList[i].emergency =
-        contactsList[i].emergency == false ? true : false;
-      if (contactsList[i].emergency) {
+      
+      flag == "update"
+        ? contactsList[i].emergency == contactsList[i].emergency
+        : (contactsList[i].emergency =
+            contactsList[i].emergency == false ? true : false);
+      if ((contactsList[i].emergency && (emergancyList.length !== 0 && emergancyList[i].id !== id)) ||  emergancyList.length == 0) {
         emergancyList.push(contactsList[i]);
         localStorage.setItem("emgList", JSON.stringify(emergancyList));
         localStorage.setItem("listOfContact", JSON.stringify(contactsList));
         displayContats(contactsList);
-      } else {
+      }else if (contactsList[i].emergency &&(emergancyList.length !==0 && emergancyList[i].id == id)){
+        displayContats(contactsList);
+      }
+       else {
         localStorage.setItem("listOfContact", JSON.stringify(contactsList));
         removeEmergancy(id);
       }
@@ -315,7 +330,7 @@ function addEmergancy(id) {
   }
 }
 function removeEmergancy(id) {
-  for (let i = 0; i < emergancyList.length; i++) {
+  for (var i = 0; i < emergancyList.length; i++) {
     if (emergancyList[i].id == id) {
       emergancyList.splice(i, 1);
       localStorage.setItem("emgList", JSON.stringify(emergancyList));
@@ -326,26 +341,194 @@ function removeEmergancy(id) {
 
 function searchContact() {
   var searchTerm = searchInput.value;
-
-  var filterContact = [];
-
-  for (let i = 0; i < contactsList.length; i++) {
-    if (
-      contactsList[i].name
-        .toLocaleLowerCase()
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      contactsList[i].phone
-        .toLocaleLowerCase()
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      contactsList[i].email
-        .toLocaleLowerCase()
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase())
-    ) {
+  if (searchTerm == "") {
+    filterContact = [];
+  } else {
+    for (var i = 0; i < contactsList.length; i++) {
+      if (
+        contactsList[i].name
+          .toLocaleLowerCase()
+          .includes(searchTerm.toLocaleLowerCase()) ||
+        contactsList[i].phone
+          .toLocaleLowerCase()
+          .includes(searchTerm.toLocaleLowerCase()) ||
+        contactsList[i].email
+          .toLocaleLowerCase()
+          .includes(searchTerm.toLocaleLowerCase())
+      ) {
         filterContact.push(contactsList[i]);
+      }
     }
   }
+
   displayContats(filterContact);
+}
+
+function deleteContact(id) {
+  Swal.fire({
+    title: "Delete Contact?",
+    text: "Are you sure you want to delete Wayne Avery? This action cannot be undone.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed)
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+    for (var i = 0; i < contactsList.length; i++) {
+      if (contactsList[i].id == id) {
+        contactsList.splice(i, 1);
+        localStorage.setItem("listOfContact", JSON.stringify(contactsList));
+      }
+    }
+    for (var i = 0; i < favouriteList.length; i++) {
+      if (favouriteList[i].id == id) {
+        favouriteList.splice(i, 1);
+        localStorage.setItem("favList", JSON.stringify(favouriteList));
+      }
+    }
+    for (var i = 0; i < emergancyList.length; i++) {
+      if (emergancyList[i].id == id) {
+        emergancyList.splice(i, 1);
+        localStorage.setItem("emgList", JSON.stringify(emergancyList));
+      }
+    }
+    displayContats(contactsList);
+  });
+}
+
+function getInitials(name) {
+  return name
+    .split(" ")
+    .map((w) => w[0])
+    .join("")
+    .toUpperCase();
+}
+
+function displayFavContacts() {
+  var cartona = "";
+  if (favouriteList.length == 0) {
+    cartona = `<div class="div text-center">No favorites yet</div>`;
+  }
+  for (var i = 0; i < favouriteList.length; i++) {
+    cartona += `
+        <div
+                      class="contact-item d-flex align-items-center justify-content-between"
+                    >
+                      <!-- Left -->
+                      <div class="d-flex align-items-center gap-3">
+                        <!-- Avatar -->
+                        <div class="avatar">${getInitials(favouriteList[i].name)}</div>
+
+                        <!-- Info -->
+                        <div>
+                          <h6 class="mb-0 fw-bold">${favouriteList[i].name}</h6>
+                          <small class="text-muted">${favouriteList[i].phone}</small>
+                        </div>
+                      </div>
+
+                      <!-- Right Icon -->
+                      <div class="call-btn">
+                        <i class="fa-solid fa-phone"></i>
+                      </div>
+                    </div>
+        `;
+  }
+  document.getElementById("rowFavorite").innerHTML = cartona;
+}
+
+function displayEmgContacts() {
+  var cartona = "";
+  if (emergancyList.length == 0) {
+    cartona = `<div class="div text-center ">No emergency contacts</div>`;
+  }
+  for (var i = 0; i < emergancyList.length; i++) {
+    cartona += `
+        <div
+                      class="contact-item-emr d-flex align-items-center justify-content-between"
+                    >
+                      <div class="d-flex align-items-center gap-3">
+                        <div class="avatar">${getInitials(emergancyList[i].name)}</div>
+
+                        <div>
+                          <h6 class="mb-0 fw-bold">${emergancyList[i].name}</h6>
+                          <small class="text-muted">${emergancyList[i].phone}</small>
+                        </div>
+                      </div>
+
+                      <div class="call-btn emr-btn">
+                        <i class="fa-solid fa-phone"></i>
+                      </div>
+                    </div>
+        `;
+  }
+  document.getElementById("rowEmergancy").innerHTML = cartona;
+}
+var updateIndex = 0;
+function putContactDate(index) {
+  updateID = contactsList[index].id;
+  nameInput.value = contactsList[index].name;
+  phoneInput.value = contactsList[index].phone;
+  emailInput.value = contactsList[index].email;
+  addressInput.value = contactsList[index].address;
+  groupInput.value = contactsList[index].group;
+  noteInput.value = contactsList[index].note;
+  favouriteInput.checked = contactsList[index].favourite;
+  emergencyInput.checked = contactsList[index].emergency;
+
+  document.getElementById("updateContact").classList.add("d-block");
+  document.getElementById("updateContact").classList.remove("d-none");
+  document.getElementById("saveContact").classList.add("d-none");
+
+  var modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+  modal.show();
+}
+
+function updateContact() {
+  for (var i = 0; i < contactsList.length; i++) {
+    if (contactsList[i].id == updateID) {
+      contactsList[i].name = nameInput.value;
+      contactsList[i].phone = phoneInput.value;
+      contactsList[i].email = emailInput.value;
+      contactsList[i].address = addressInput.value;
+      contactsList[i].group = groupInput.value;
+      contactsList[i].note = noteInput.value;
+      contactsList[i].favourite = favouriteInput.checked;
+      contactsList[i].emergency = emergencyInput.checked;
+
+      if (contactsList[i].favourite == false) {
+        removeFavourite(updateID);
+      } else {
+        addFavourite(updateID, "update");
+      }
+      if (contactsList[i].emergency == false) {
+        removeEmergancy(updateID);
+      } else {
+        addEmergancy(updateID, "update");
+      }
+    }
+    localStorage.setItem("listOfContact", JSON.stringify(contactsList));
+
+    displayContats(contactsList);
+    displayFavContacts();
+    displayEmgContacts();
+  }
+
+  updateID = 0;
+  var modal = bootstrap.Modal.getInstance(
+    document.getElementById("exampleModal"),
+  );
+  modal.hide();
+  resetAllInputs();
+   Swal.fire({
+      title: "Updated!",
+      text: "Contact has been updated successfully.",
+      icon: "success",
+      draggable: true,
+    });
 }
